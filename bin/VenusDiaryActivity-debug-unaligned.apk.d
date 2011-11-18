85e6bf2ae442fff50d87d7b5dd71db6603dfc2ed
Jason Berlinsky <jberlinsky@gmail.com>
2011-11-18 13:03:02 -0500

    Update project for CLI work

---
+++ bin/VenusDiaryActivity-debug-unaligned.apk.d
@@ -0,0 +1,3 @@
+/Users/jasonberlinsky/Code/P-G-Venus/bin/VenusDiaryActivity-debug-unaligned.apk : \
+/Users/jasonberlinsky/Code/P-G-Venus/bin/VenusDiaryActivity.ap_ \
+/Users/jasonberlinsky/Code/P-G-Venus/bin/classes.dex \
